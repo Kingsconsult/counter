43960e630a51095137d2f25ee265a3f9b47f762b
kingsconsult <kingsconsult001@gmail.com>
2020-12-09 01:53:51 +0100

    completed the counter

---
+++ src/views/About.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <Counter
+      :year="2021"
+      :month="0"
+      :date="0"
+      :hour="0"
+      :minute="0"
+      :second="0"
+      :millisecond="0"
+    />
+  </div>
+</template>
+
+<script>
+import Counter from "@/components/Counter.vue";
+export default {
+  components: { Counter },
+};
+</script>
+
+<style  scoped>
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="home">
+    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Counter
+      :year="2021"
+      :month="0"
+      :date="0"
+      :hour="0"
+      :minute="0"
+      :second="0"
+      :millisecond="0" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+import Counter from '@/components/Counter.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    // HelloWorld,
+    Counter
+  }
+}
+</script>
